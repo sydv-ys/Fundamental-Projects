5c0744fa31de8a6dbfadf825c715b64981310e37
sydv-ys <Aysu.Seyidova@Kibrit.Tech>
2025-01-28 18:04:18 +0400

    updates

--- Grocery Bud/src/App.jsx
+++ Grocery Bud/src/App.jsx
@@ -15,12 +15,15 @@ const App = () => {
     setItems([...items, newItem]);
   };
 
-  const removeItem = (itemId) => {};
+  const removeItem = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId);
+    setItems(newItems);
+  };
 
   return (
     <section className="section-center">
       <Form addItem={addItem} />
-      <Items items={items} removeItem={removeItem}/>
+      <Items items={items} removeItem={removeItem} />
     </section>
   );
 };
--- Grocery Bud/src/SingleItem.jsx
+++ Grocery Bud/src/SingleItem.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function SingleItem({item, removeItem}) {
+function SingleItem({ item, removeItem }) {
+  const [isChecked, setIsChecked] = useState(item.completed);
+
   return (
-    <div className='single-item'>
-        <input type="checkbox" />
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p>{item.name}</p>
+      <button className="btn remove-btn">delete</button>
     </div>
-  )
+  );
 }
 
-export default SingleItem
+export default SingleItem;
