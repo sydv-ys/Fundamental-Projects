6f2814777d2821018606ff17e661d4000e1939cd
sydv-ys <Aysu.Seyidova@Kibrit.Tech>
2025-01-31 10:30:18 +0400

    local storage fixed

--- Grocery Bud/src/App.jsx
+++ Grocery Bud/src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from "react";
 import Form from "./Form";
 import { nanoid } from "nanoid";
 import Items from "./Items";
-import SingleItem from "./SingleItem";
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if (list) {
+    list = JSON.parse(localStorage.getItem('list'));
+  } else {
+    list = [];
+  }
+  return list;
+};
 
 const setLocalStorage = (items) => {
   localStorage.setItem("list", JSON.stringify(items));
 };
 
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getLocalStorage());
 
   const addItem = (itemName) => {
     const newItem = {
