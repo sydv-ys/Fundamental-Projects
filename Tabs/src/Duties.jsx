c36d033ac7cfa842596cf7e24f8dd07979d70a58
sydv-ys <Aysu.Seyidova@Kibrit.Tech>
2025-02-19 18:03:49 +0400

    clean up

--- Tabs/src/Duties.jsx
+++ Tabs/src/Duties.jsx
@@ -7,7 +7,6 @@ function Duties({ duties }) {
     <div>
       {duties.map((duty, index) => {
         const id = v4();
-        console.log(id);
         return (
           <div key={index} className="job-disc">
             <FaAngleDoubleRight className="job-icon" />
