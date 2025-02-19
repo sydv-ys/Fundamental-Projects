e540472e222cf197e3f972e43ffa02c4b98bf08f
sydv-ys <Aysu.Seyidova@Kibrit.Tech>
2025-02-19 12:09:52 +0400

    project

--- Qusetions/src/SingleQuestion.jsx
+++ Qusetions/src/SingleQuestion.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-function SingleQuestion() {
+function SingleQuestion({ title, info }) {
+  const [showInfo, setShowInfo] = useState(false);
+
   return (
-    <>
-    SingleQuestion
-    </>
+    <article className="question">
+      <header>
+        <h5>{title}</h5>
+        <button className="question-btn" onClick={() => setShowInfo(!showInfo)}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p>{info}</p>}
+    </article>
   );
 }
 
